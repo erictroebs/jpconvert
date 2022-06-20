632b50431d8810de318714ee0194e6a9fd83b11d
Eric Tröbs <eric.troebs@tu-ilmenau.de>
2022-06-20 10:29:12 +0200

    0.2.2: add Dockerfile

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
 
 setup(
     name='jpconvert',
-    version='0.2.1',
+    version='0.2.2',
     author='Eric Tröbs',
     author_email='eric.troebs@tu-ilmenau.de',
     description='macros for Jupyter Notebooks',
