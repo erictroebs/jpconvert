80342028092ba01e6825bd3ab96d52bda3b89a57
Eric Tröbs <1076344+erictroebs@users.noreply.github.com>
2023-04-24 13:05:05 +0200

    0.3.13: fix an exception caused by ordering imports

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
 
 setup(
     name='jpconvert',
-    version='0.3.12',
+    version='0.3.13',
     author='Eric Tröbs',
     author_email='eric.troebs@tu-ilmenau.de',
     description='macros for Jupyter Notebooks',
